84486eab2aab84d9c6dc0fac90357b608e013bf5
Sheldon Johnson <shayolden@hotmail.com>
2018-02-18 23:27:11 +1100

    Rubocop compliance

--- spec/factories/mailing_list_contact.rb
+++ spec/factories/mailing_list_contact.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :mailing_list_contact do
     name "Sheldon Johnson"
--- spec/mailers/previews/mailing_list_contact_mailer_preview.rb
+++ spec/mailers/previews/mailing_list_contact_mailer_preview.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/mailing_list_contact_mailer
 class MailingListContactMailerPreview < ActionMailer::Preview
-
 end
