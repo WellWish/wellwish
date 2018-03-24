7e112ecc0b609d50a0156e86d52e8bd05f673385
Sheldon Johnson <shayolden@hotmail.com>
2018-03-24 14:54:37 +1100

    WIP

--- spec/features/add_wishes_spec.rb
+++ spec/features/add_wishes_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "AddWishes", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "A user adds a wish" do
+    binding.irb
+    sign_in users(:ryan)
+  end
 end
