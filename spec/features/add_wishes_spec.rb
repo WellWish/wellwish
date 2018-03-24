49b89cc2b283f7eae97c97878f6d50cb38484c49
Sheldon Johnson <shayolden@hotmail.com>
2018-03-24 19:20:35 +1100

    Add add_wish feature spec

--- spec/features/add_wishes_spec.rb
+++ spec/features/add_wishes_spec.rb
@@ -1,8 +1,21 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.feature "AddWishes", type: :feature do
   scenario "A user adds a wish" do
-    binding.irb
-    sign_in users(:ryan)
+    user = create(:user)
+
+    sign_in user
+    click_on "Create Wish"
+    fill_in "Title", with: "Fix the broken basketball hoop"
+    fill_in "Description", with: "Somebody dunked the ring and now it's bent " \
+      "down. Would be good if someone could bend it back into shape."
+    fill_in "Location", with: "Gowrie Street Reserve, Newtown, NSW, Australia"
+    click_on "Add tools"
+    fill_in "Tool", with: "ladder"
+    click_on "Submit"
+
+    expect(page).to have_text("Wish was created")
   end
 end
