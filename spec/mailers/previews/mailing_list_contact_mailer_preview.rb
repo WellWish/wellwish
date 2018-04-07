30befa7e32a15b670947dc0037ab2684d54ace1f
Sheldon Johnson <shayolden@hotmail.com>
2018-04-07 20:50:53 +1000

    add mailing list contact preview

--- spec/mailers/previews/mailing_list_contact_mailer_preview.rb
+++ spec/mailers/previews/mailing_list_contact_mailer_preview.rb
@@ -2,4 +2,12 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/mailing_list_contact_mailer
 class MailingListContactMailerPreview < ActionMailer::Preview
+  def confirmation_email
+    @mailing_list_contact = MailingListContact.create(
+      name: "Sheldon",
+      email: "sheldon@wellwish.org"
+    )
+
+    MailingListContactMailer.confirmation_email(@mailing_list_contact)
+  end
 end
