a4674641cfa0a192a1f46be8104c345184fd1cc8
Sheldon Johnson <shayolden@hotmail.com>
2018-04-14 12:38:35 +1000

    Add attributes to wishes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324031336) do
+ActiveRecord::Schema.define(version: 20180414023629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20180324031336) do
   create_table "wishes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "description"
+    t.string "location"
   end
 
 end
