e535c2e47f1148b95e67d7fca17de63536844528
Sheldon Johnson <shayolden@hotmail.com>
2018-04-14 13:13:20 +1000

    Add wish factory

---
+++ spec/factories/wishes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :wish do
+    title "Fix the broken basketball ring"
+    description("The basketballl ring has been dunked and bent down. Needs" \
+                "somebody to bend back in to shape")
+  end
+end
