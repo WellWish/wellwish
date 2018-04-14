00110da1a4887a793ef892f72f3de04772c1d87b
Sheldon Johnson <shayolden@hotmail.com>
2018-04-14 13:10:19 +1000

    Add title, description and location to wishes

--- db/migrate/20180414023629_add_attributes_to_wishes.rb
+++ db/migrate/20180414023629_add_attributes_to_wishes.rb
@@ -1,7 +1,7 @@
 class AddAttributesToWishes < ActiveRecord::Migration[5.1]
   def change
     add_column :wishes, :title, :string
-    add_column :wishes, :description, :string
+    add_column :wishes, :description, :text
     add_column :wishes, :location, :string
   end
 end
